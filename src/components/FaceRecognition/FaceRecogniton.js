5239d244960f1268499a75353056b573f7bb8e0e
Anquab <anquabkhan@gmail.com>
2022-03-01 21:55:30 +0530

    test2

--- src/components/FaceRecognition/FaceRecogniton.js
+++ src/components/FaceRecognition/FaceRecogniton.js
@@ -7,7 +7,6 @@ const FaceRecognition = ({ imageurl, box }) => {
             <div className="absolute mt2">
             <img id= 'inputimage' src= { imageurl } alt="img not found" width='700px'/>   
                 <div className="bounding-box" style= {{top: box.toprow , bottom: box.bottomrow, right: box.rightcol, left: box.leftcol }}>                          
-                <p></p>
             </div>            
             </div>
                 
